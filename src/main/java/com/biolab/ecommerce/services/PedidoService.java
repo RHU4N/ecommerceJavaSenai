403bdb0d2b7712103c160090552b9fb828e53ff3
RHU4N <rhuan3003@gmail>
2026-08-26 11:12:03 -0300

    Fix:status e momento pedido

--- src/main/java/com/biolab/ecommerce/services/PedidoService.java
+++ src/main/java/com/biolab/ecommerce/services/PedidoService.java
@@ -3,9 +3,12 @@ package com.biolab.ecommerce.services;
 import com.biolab.ecommerce.DTOs.PedidoDTO;
 import com.biolab.ecommerce.entities.Pedido;
 import com.biolab.ecommerce.entities.Usuario;
+import com.biolab.ecommerce.entities.enums.StatusPedido;
 import com.biolab.ecommerce.repositories.PedidoRepository;
 import com.biolab.ecommerce.repositories.UsuarioRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 public class PedidoService {
@@ -22,8 +25,8 @@ public class PedidoService {
         Usuario u = usuarioRepository.findById(dto.getIdCliente())
                 .orElseThrow();
         p.setCliente(u);
-        p.setStatus(dto.getStatus());
-        p.setMomento(dto.getMomento());
+        p.setStatus(StatusPedido.AGUARDANDO_PAGAMENTO);
+        p.setMomento(Instant.now());
         pedidoRepository.save(p);
         return "Pedido salvo com sucesso";
     }
