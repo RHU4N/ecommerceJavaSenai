c6e703dae1c38f3d608a6aa223c19c645e0924cb
RHU4N <rhuan3003@gmail>
2026-09-09 09:49:16 -0300

    Feat: Add entidade produto e categoria

---
+++ src/main/java/com/biolab/ecommerce/entities/Produto.java
@@ -0,0 +1,31 @@
+package com.biolab.ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private String imgUrl;
+
+    @ManyToMany
+    @JoinTable(name = "produto_categoria",
+            joinColumns = @JoinColumn(name = "produto_id"),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id")
+    )
+    private Set<Categoria> categorias = new HashSet<>();
+
+}
